eb0506c18aa5f8433dc1aa6905f9dfb421682c24
Jinsung Lim <js99028@gmail.com>
2020-10-21 16:34:57 +0900

    Add tailwind's default viewports

--- template/.storybook/preview.js
+++ template/.storybook/preview.js
@@ -2,4 +2,37 @@ import '../src/index.css'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  viewport: {
+    defaultViewport: 'sm',
+    viewports: {
+      sm: {
+        name: 'sm',
+        styles: {
+          width: '640px',
+          height: '1000px',
+        },
+      },
+      md: {
+        name: 'md',
+        styles: {
+          width: '768px',
+          height: '1000px',
+        },
+      },
+      lg: {
+        name: 'lg',
+        styles: {
+          width: '1024px',
+          height: '1000px',
+        },
+      },
+      xl: {
+        name: 'xl',
+        styles: {
+          width: '1280px',
+          height: '1000px',
+        },
+      },
+    }
+  }
 }
